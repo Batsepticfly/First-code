0093c9b5814616ab9f70e4ca05fb00a3825f44d8
Batsepticfly <bubblesrsh@gmail.com>
2017-07-03 15:20:42 -0400

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,20 @@
+name=raw_input("What is your name?")
+        
+age=raw_input("How old are you?")
+grade=raw_input("What grade?")
+interests=raw_input("What are your interests")
+print("My name is",name,"and my age is", age, "my grade is", grade, "my interests are", interests)
+
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+    
